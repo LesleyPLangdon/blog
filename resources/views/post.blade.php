d819bc4df72f96c567d3ff3ec746dd9b737cab7e
LesleyPLangdon <61936741+LesleyPLangdon@users.noreply.github.com>
2021-07-17 08:07:32 -0400

    Add bolg posts and route

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article> 
+        <h1><a href="/post">My First Post</a></h1>
+        
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quia explicabo cum amet saepe porro reprehenderit fugit, doloremque quaerat earum atque optio ipsa, voluptatum sed cumque tempora impedit placeat pariatur.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+        
+</body>
